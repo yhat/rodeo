def5fbcc184cc44ab66dfc675535f428325b47ce
glamp <lamp.greg@gmail.com>
2015-08-31 21:17:08 -0400

    find me a python

---
+++ src/steve-irwin.js
@@ -0,0 +1,80 @@
+var exec = require('child_process').exec;
+var async = require('async');
+var path = require('path');
+var fs = require('fs');
+var fse = require('fs-extra');
+var tmp = require('tmp');
+
+
+var USER_HOME = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+
+function getRC() {
+  var rodeorc = path.join(USER_HOME, ".rodeorc");
+  var rc;
+  if (fs.existsSync(rodeorc)) {
+    rc = JSON.parse(fs.readFileSync(rodeorc).toString())
+  } else {
+    rc = {};
+  }
+  return rc;
+}
+
+// Steve Irwin gets you a python, no questions asked
+function findMeAPython(fn) {
+  // /usr/bin/env python doesn't really work, so we're going to be doing the ole
+  // guess and check method
+  var testPython = path.join(__dirname, "../src", "check_python.py");
+  var testPythonFile = tmp.fileSync();
+  fse.copySync(testPython, testPythonFile.name);
+
+  var rc = getRC();
+  var pythonCmds = [];
+  // if we have one in our RC file, we'll prioritize that first
+  if (rc.pythonCmd) {
+    pythonCmds.push(rc.pythonCmd);
+  }
+  pythonCmds = pythonCmds.concat([
+    "/usr/local/bin/ipython",
+    "/anaconda/bin/python",
+    path.join(USER_HOME, "anaconda", "bin", "python"),
+    path.join(USER_HOME, "anaconda", "python"),
+    "/usr/bin/python",
+    path.join(USER_HOME, "bin", "python"),
+    "python"
+  ]);
+
+  var i = 0;
+  var pythonCmd;
+  async.whilst(
+    function() {
+      if (pythonCmds.length==i) {
+        return false;
+      } else if (pythonCmd==null) {
+        return true;
+      }
+    },
+    function(callback) {
+      pythonCmd = pythonCmds[i];
+      i++;
+      exec(pythonCmd + " " + testPythonFile.name, function(err, stdout, stderr) {
+        if (err) {
+          pythonCmd = null;
+        } else if (stdout.toString()!="FAIL") {
+          // then we're good
+        } else {
+          pythonCmd = null;
+        }
+        callback();
+      });
+    },
+    function(err) {
+      fn(err, pythonCmd);
+    }
+  );
+};
+
+// findMeAPython(function(err, cmd) {
+//   console.log(cmd);
+// })
+
+module.exports.findMeAPython = findMeAPython;
