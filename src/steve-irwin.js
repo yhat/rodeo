3e662c5643f9b795bd3583580b0594e1fbc8c938
glamp <lamp.greg@gmail.com>
2015-09-08 17:39:26 -0400

    should fix #14 and possibly #13

--- src/steve-irwin.js
+++ src/steve-irwin.js
@@ -19,44 +19,69 @@ function findMeAPython(fn) {
   if (rc.pythonCmd) {
     pythonCmds.push(rc.pythonCmd);
   }
-  pythonCmds = pythonCmds.concat([
-    "/usr/local/bin/ipython",
-    "/anaconda/bin/python",
-    path.join(USER_HOME, "anaconda", "bin", "python"),
-    path.join(USER_HOME, "anaconda", "python"),
-    "/usr/bin/python",
-    path.join(USER_HOME, "bin", "python"),
-    "python"
-  ]);
 
-  var i = 0;
-  var pythonCmd;
-  async.whilst(
-    function() {
-      if (pythonCmds.length==i) {
-        return false;
-      } else if (pythonCmd==null) {
-        return true;
-      }
-    },
-    function(callback) {
-      pythonCmd = pythonCmds[i];
-      i++;
-      exec(pythonCmd + " " + testPythonFile.name, function(err, stdout, stderr) {
-        if (err) {
-          pythonCmd = null;
-        } else if (stdout.toString()!="FAIL") {
-          // then we're good
-        } else {
-          pythonCmd = null;
+  var cmd;
+  if (/^win/.test(process.platform)) {
+    cmd = "echo %path%";
+  } else {
+    cmd = "source ~/.bash_profile > /dev/null && echo $PATH";
+  }
+  exec(cmd, function(err, stdout, stderr) {
+    var opts = {};
+    if (stdout) {
+      var userPath = stdout.toString().trim();
+      opts = {
+        env: {
+          PATH: userPath
         }
-        callback();
-      });
-    },
-    function(err) {
-      fn(err, pythonCmd);
+      };
     }
-  );
+    opts = {}
+    exec("python -c 'import sys; print(sys.executable)'", opts, function(err, stdout, stderr) {
+      if (opts.env && stdout) {
+        pythonCmds.push(stdout.toString().trim());
+      }
+
+      pythonCmds = pythonCmds.concat([
+        "/usr/local/bin/ipython",
+        "/anaconda/bin/python",
+        path.join(USER_HOME, "anaconda", "bin", "python"),
+        path.join(USER_HOME, "anaconda", "python"),
+        "/usr/bin/python",
+        path.join(USER_HOME, "bin", "python"),
+        "python"
+      ]);
+
+      var i = 0;
+      var pythonCmd;
+      async.whilst(
+        function() {
+          if (pythonCmds.length==i) {
+            return false;
+          } else if (pythonCmd==null) {
+            return true;
+          }
+        },
+        function(callback) {
+          pythonCmd = pythonCmds[i];
+          i++;
+          exec(pythonCmd + " " + testPythonFile.name, function(err, stdout, stderr) {
+            if (err) {
+              pythonCmd = null;
+            } else if (stdout.toString()!="FAIL") {
+              // then we're good
+            } else {
+              pythonCmd = null;
+            }
+            callback();
+          });
+        },
+        function(err) {
+          fn(err, pythonCmd);
+        }
+      );
+    });
+  });
 };
 
 module.exports.findMeAPython = findMeAPython;
