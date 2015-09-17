6fcb4276b96a6a482dfec15ab5876155d9104fcf
glamp <lamp.greg@gmail.com>
2015-09-17 11:03:04 -0400

    release scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,15 +11,19 @@ electron-builder build/win32/all/Rodeo-win32-ia32 --platform=win \
 electron-builder build/win32/all/Rodeo-win32-x64 --platform=win \
         --out=./build/win32/all/Rodeo-win32-x64 --config=packager.json
 
-echo "Release name? "
-read NAME
-echo "Release tag? "
-read TAG
-echo "Uploading release to GitHub:"
-echo "  NAME: ${NAME}"
-echo "  TAG: ${TAG}"
-github-release release -u yhat -r rodeo-native -n "${NAME}" -d "${NAME}" --tag ${TAG}
-github-release upload -u yhat -r rodeo-native --tag ${TAG} --name "Rodeo-mac.dmg" --file build/darwin/x64/Rodeo-darwin-x64/Rodeo.dmg
-github-release upload -u yhat -r rodeo-native --tag ${TAG} --name "RodeoSetup-ia32.exe" --file build/win32/all/Rodeo-win32-ia32/Rodeo\ Setup.exe
-github-release upload -u yhat -r rodeo-native --tag ${TAG} --name "RodeoSetup-x64.exe" --file build/win32/all/Rodeo-win32-x64/Rodeo\ Setup.exe
 
+if [ "$1"!="" ]; then
+
+  echo "Release name? "
+  read NAME
+  echo "Release tag? "
+  read TAG
+  echo "Uploading release to GitHub:"
+  echo "  NAME: ${NAME}"
+  echo "  TAG: ${TAG}"
+  github-release release -u yhat -r rodeo-native -n "${NAME}" -d "${NAME}" --tag ${TAG}
+  github-release upload -u yhat -r rodeo-native --tag ${TAG} --name "Rodeo-mac.dmg" --file build/darwin/x64/Rodeo-darwin-x64/Rodeo.dmg
+  github-release upload -u yhat -r rodeo-native --tag ${TAG} --name "RodeoSetup-ia32.exe" --file build/win32/all/Rodeo-win32-ia32/Rodeo\ Setup.exe
+  github-release upload -u yhat -r rodeo-native --tag ${TAG} --name "RodeoSetup-x64.exe" --file build/win32/all/Rodeo-win32-x64/Rodeo\ Setup.exe
+
+fi
