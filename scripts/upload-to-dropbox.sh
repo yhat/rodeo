a9b72189bcf3788cfd70668b7d063fee75ee3d8e
glamp <lamp.greg@gmail.com>
2015-10-02 14:42:45 -0400

    keep versions handy

--- scripts/upload-to-dropbox.sh
+++ scripts/upload-to-dropbox.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+set -ex
+
+
 cp build/darwin/x64/Rodeo-darwin-x64/Rodeo.dmg ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/Rodeo-mac.dmg
 cp build/win32/all/Rodeo-win32-x64/Rodeo\ Setup.exe ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/Rodeo-win32-x64.exe
 cp build/win32/all/Rodeo-win32-ia32/Rodeo\ Setup.exe ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/Rodeo-win32-ia32.exe
+
+VERSION=$(cat package.json | jq .version --raw-output)
+
+mkdir ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/${VERSION}
+cp ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/* ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/${VERSION}
