4275e787e0c4418b4109670b1c0799a5fcf97509
glamp <lamp.greg@gmail.com>
2015-10-04 13:44:53 -0400

    build stuff

--- scripts/upload-to-dropbox.sh
+++ scripts/upload-to-dropbox.sh
@@ -8,6 +8,8 @@ cp build/win32/all/Rodeo-win32-x64/Rodeo\ Setup.exe ~/Dropbox-Yhat/yhat-box/greg
 cp build/win32/all/Rodeo-win32-ia32/Rodeo\ Setup.exe ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/Rodeo-win32-ia32.exe
 
 VERSION=$(cat package.json | jq .version --raw-output)
+SHA=$(git rev-parse HEAD | awk '{ print substr($1, 0, 7)}')
 
 mkdir ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/${VERSION}
 cp ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/* ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/${VERSION}
+echo "Rodeo v${VERSION} - ${SHA}" > ~/Dropbox-Yhat/yhat-box/greg/rodeo/dist/${VERSION}/README
