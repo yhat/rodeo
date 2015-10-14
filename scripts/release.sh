237660d4147867c1246386abd13db4bca1b8d8fb
glamp <lamp.greg@gmail.com>
2015-10-14 13:40:17 -0400

    codesigning

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,11 @@
 # osx
 electron-builder build/darwin/x64/Rodeo-darwin-x64/Rodeo.app --platform=osx \
         --out=./build/darwin/x64/Rodeo-darwin-x64/ --config=packager.json
+# codesign for OSX
+codesign --deep --force --verbose --sign "Yhat, Inc." build/darwin/x64/Rodeo-darwin-x64/Rodeo.app
+codesign --verify -vvvv build/darwin/x64/Rodeo-darwin-x64/Rodeo.app
+spctl -a -vvvv build/darwin/x64/Rodeo-darwin-x64/Rodeo.app/
+
 # windows
 #   32 bit
 electron-builder build/win32/all/Rodeo-win32-ia32 --platform=win \
