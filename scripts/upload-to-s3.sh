45c62946769fdbb7d5970421638e4a3eaab45509
glamp <lamp.greg@gmail.com>
2015-11-16 18:21:56 -0500

    dmg upload

--- scripts/upload-to-s3.sh
+++ scripts/upload-to-s3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VERSION=$(cat package.json | jq -r .version)
+s3cmd -P put build/darwin/x64/Rodeo-darwin-x64/Rodeo.dmg "s3://rodeo-releases/${VERSION}/Rodeo-v${VERSION}-darwin_64.dmg"
 s3cmd -P put build/darwin/x64/Rodeo-darwin-x64/Rodeo.zip "s3://rodeo-releases/${VERSION}/Rodeo-v${VERSION}-darwin_64.zip"
 s3cmd -P put build/win32/all/Rodeo-win32-ia32.zip "s3://rodeo-releases/${VERSION}/Rodeo-v${VERSION}-windows_32.zip"
 s3cmd -P put build/win32/all/Rodeo-win32-x64.zip "s3://rodeo-releases/${VERSION}/Rodeo-v${VERSION}-windows_64.zip"
