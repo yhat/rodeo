d2cef6d63c06150f3de1b65bdf2ddd425ad8c7d7
glamp <lamp.greg@gmail.com>
2015-12-29 11:00:21 -0600

    possible windows bug

--- src/rodeo/kernel.js
+++ src/rodeo/kernel.js
@@ -74,6 +74,7 @@ function spawnPython(cmd, opts, done) {
     var payload = { async: false, id: uuid.v4().toString(), code: cmd, complete: complete };
     var output = "";
     completionCallbacks[payload.id] = function(result) {
+      console.log("[EXECUTE RESULT]: ", result);
       // autocompleted results come back as a proper JSON array
       if (complete==true) {
         output = result.output;
