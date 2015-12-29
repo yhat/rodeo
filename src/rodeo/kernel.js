3812cdfd64637bf2c8bb41a37f1e993ea8059d66
glamp <lamp.greg@gmail.com>
2015-12-29 11:43:33 -0600

    excessive logging

--- src/rodeo/kernel.js
+++ src/rodeo/kernel.js
@@ -120,7 +120,6 @@ module.exports.startNewKernel = function(pythonCmd, cb) {
     });
   } else {
     testPythonPath(pythonCmd, function(err, result) {
-      console.log("testPythonPath: ", err, result);
       if (err) {
         cb(err, { spawnfile: pythonCmd });
       } else if (result.jupyter==false) {
