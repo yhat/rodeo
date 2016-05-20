4f827c42f594ea872eefb7454f0745afc0a6794c
Dane Stuckel <dane.stuckel@gmail.com>
2016-05-20 11:21:51 -0400

    working nvm and pyenv on circle

--- scripts/osx/install-deps.sh
+++ scripts/osx/install-deps.sh
@@ -2,13 +2,13 @@
 # see https://github.com/electron-userland/electron-builder/wiki/Multi-Platform-Build
 
 # install nvm
-if [ ! -d "~/.nvm" ]; then
+if [[ ! -d ~/.nvm ]]; then
   touch ~/.bash_profile
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
   . ~/.nvm/nvm.sh
 fi
 
-if [ ! -d "~/.pyenv" ]; then
+if [[ ! -d ~/.pyenv ]]; then
   touch ~/.bash_profile
   git clone https://github.com/yyuu/pyenv.git ~/.pyenv
   echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
