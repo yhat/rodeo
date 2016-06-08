9208d8f043e488e3e01863a48f611ca41bf94ad8
coristig <colin@yhathq.com>
2016-06-08 08:46:42 -0400

    added text to right of run script button

--- src/browser/jsx/containers/studio-layout/studio-layout.jsx
+++ src/browser/jsx/containers/studio-layout/studio-layout.jsx
@@ -155,6 +155,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(React.createClass({
             <li className="right">
               <a className="not-tab" onClick={props.onRunActiveAcePane} title="Run script">
                 <span className="fa fa-play-circle" />
+                <span className="right-icon-text">{'Run Script'}</span>
               </a>
             </li>
 
