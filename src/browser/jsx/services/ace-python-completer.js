459c3135561db56cb96a10cdb64b1868c743b576
Dane Stuckel <dane.stuckel@gmail.com>
2016-07-25 19:41:33 -0400

    better docstrings

--- src/browser/jsx/services/ace-python-completer.js
+++ src/browser/jsx/services/ace-python-completer.js
@@ -6,6 +6,68 @@ import AsciiToHtml from 'ansi-to-html';
 
 const convertor = new AsciiToHtml();
 
+/**
+ * @param {[string]} matches
+ * @param {string} prefix
+ * @returns {[string]}
+ */
+function filterByStartingText(matches, prefix) {
+  return _.filter(matches, match => _.startsWith(match, prefix));
+}
+
+function handleMatch(context) {
+  const prefix = context.prefix,
+    matchPrefix = context.matchPrefix;
+
+  return function (match) {
+    let value = match,
+      id = cid(),
+      score = 100,
+      meta;
+
+    if (_.startsWith(match, matchPrefix)) {
+      value = prefix + match.substr(matchPrefix.length);
+    }
+
+    // if it's not a filename and there's a '.' in the value, we want
+    // to set the value to just the last item in the list
+    if (value.indexOf('/') == -1 && value.indexOf('.') > -1) {
+      value = value.split('.').slice(value.split('.').length - 1).join('.');
+    }
+
+    if (value !== match) {
+      meta = match;
+    }
+
+    return {
+      rodeoExtras: _.assign({id, match}, context),
+      caption: value, // thing shown on left
+      value,
+      score,
+      meta // thing shown on right
+    };
+  };
+}
+
+/**
+ * @param {string} id
+ * @param {string} value
+ * @returns {Promise}
+ */
+function fillAutocompleteElementById(id, value) {
+  return client.executeHidden('?' + value, 'execute_reply').then(function (result) {
+    const text = _.get(result, 'payload[0].data["text/plain"]'),
+      el = document.querySelector('#' + id);
+
+    if (text && el) {
+      el.style.display = 'block';
+      el.style.opacity = 0;
+      el.innerHTML = convertor.toHtml(text);
+      _.defer(() => el.style.opacity = 1);
+    }
+  });
+}
+
 /* eslint max-params: 0 */
 export default {
   /**
@@ -35,73 +97,32 @@ export default {
         }});
 
       // remove matches that are not really matches
-      if (prefix) {
-        result.matches = _.filter(result.matches, function (match) {
-          return _.startsWith(match, matchPrefix);
-        });
+      if (prefix && matchPrefix) {
+        result.matches = filterByStartingText(result.matches, matchPrefix);
       }
 
-      callback(null, _.map(result.matches, function (match) {
-        let value = match,
-          id = cid(),
-          meta;
-
-        if (_.startsWith(match, matchPrefix)) {
-          value = prefix + match.substr(matchPrefix.length);
-        }
-
-        // if it's not a filename and there's a '.' in the value, we want
-        // to set the value to just the last item in the list
-        if (value.indexOf('/') == -1 && value.indexOf('.') > -1) {
-          value = value.split('.').slice(value.split('.').length - 1).join('.');
-        }
-
-        if (value !== match) {
-          meta = match;
-        }
-
-        return {
-          rodeoExtras: {
-            id,
-            prefix,
-            pos,
-            cursorPos,
-            result,
-            match,
-            code
-          },
-          docHTML: '<span id="autocomplete-' + id + '" />',
-          caption: value, // thing shown on left
-          value,
-          score: 100,
-          meta // thing shown on right
-          // docHTML: '<code>' + match + '</code>' + '<br/>' + '<br/>' + '<pre style=\'margin: 0; padding: 0;\'>' +
-          //   p.docstring + '</pre>' || '<p>' + match + '</p>'
-          // snippet: match, // thing that can replace section
-        };
-      }));
+      callback(null, _.map(result.matches, handleMatch({
+        prefix,
+        postfix,
+        pos,
+        cursorPos,
+        result,
+        code
+      })));
     }).catch(error => console.error(error));
   },
 
   getDocTooltip: function (item) {
-    console.log('getDocTooltip', this, arguments);
-
     const extras = item.rodeoExtras,
       id = 'autocomplete-result-' + extras.id;
 
-    client.executeHidden('?' + item.caption, 'execute_reply').then(function (result) {
-      const text = _.get(result, 'payload[0].data["text/plain"]'),
-        el = document.querySelector('#' + id);
-      
-      if (text && el) {
-        el.style.display = 'block';
-        el.style.opacity = 0;
-        el.innerHTML = convertor.toHtml(text);
-        _.defer(() => el.style.opacity = 1);
-      }
-    }).catch(error => console.error('errrrrr', error));
+    // no return here, allow to fail, report failure
+    fillAutocompleteElementById(id, extras.match)
+      .catch(error => console.error('errrrrr', error));
 
-    return {docHTML: '<div class="rodeo-ace-pane-docstring" style="overflow: auto; display: none;" id="' + id + '"></div>'};
+    return {
+      docHTML: '<div class="rodeo-ace-pane-docstring" style="overflow: auto; display: none;" id="' + id + '"></div>'
+    };
   }
 };
 
